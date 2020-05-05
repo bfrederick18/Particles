94fe90b5d0b3f736eb1aa1b9f955af34adecc5b9
bfrederick18 <1ultimatedj@gmail.com>
2020-05-04 22:23:45 -0700

    Added AcceleratingSprite and ran game

---
+++ Particles/Particles/AcceleratingSprite.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Particles
+{
+    public class AcceleratingSprite : MovingSprite
+    {
+        protected Vector2 acceleration;
+
+        public AcceleratingSprite(Texture2D texture, Vector2 position, Color tint, Vector2 scale, Vector2 velocity, Vector2 acceleration)
+           : base(texture, position, tint, scale, velocity)
+        {
+            this.acceleration = acceleration;
+        }
+
+        public AcceleratingSprite(Texture2D texture, Vector2 position, Vector2 scale, Vector2 velocity, Vector2 acceleration)
+           : this(texture, position, Color.White, scale, velocity, acceleration)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime, Viewport screen)
+        {
+            velocity += acceleration;
+            base.Update(gameTime, screen);
+        }
+    }
+}
--- Particles/Particles/Game1.cs
+++ Particles/Particles/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Net;
 
 namespace Particles
 {
@@ -8,6 +9,9 @@ namespace Particles
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        BaseSprite middlePx;
+        AcceleratingSprite px;
 
         public Game1()
         {
@@ -17,17 +21,27 @@ namespace Particles
 
         protected override void Initialize()
         {
+            graphics.PreferredBackBufferWidth = 3000;
+            graphics.PreferredBackBufferHeight = 1900;
+
+            graphics.ApplyChanges();
+
+            IsMouseVisible = true;
+
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            middlePx = new BaseSprite(Content.Load<Texture2D>("Textures/white2By2"), new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2), Color.Red, Vector2.One);
+
+            px = new AcceleratingSprite(Content.Load<Texture2D>("Textures/white2By2"), new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2), new Vector2(100f, 100f), Vector2.Zero, new Vector2(0.1f, 0.1f));
         }
 
         protected override void UnloadContent()
         {
-
+            Content.Unload();
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,13 +49,21 @@ namespace Particles
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            middlePx.Update(gameTime, GraphicsDevice.Viewport);
+            px.Update(gameTime, GraphicsDevice.Viewport);
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin();
 
+            px.Draw(spriteBatch);
+
+            middlePx.Draw(spriteBatch);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
